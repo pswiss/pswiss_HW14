b4d35752a8b47c20c5fd3a765e7165faa9151acd
pswiss <petrasswissler2022@u.northwestern.edu>
2017-05-13 14:08:40 -0500

    Restarted Pic Project to be the base version

---
+++ pswiss_HW14Code/firmware/pswiss_HW14Code.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=pswiss_HW14Code.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/pswiss_HW14Code.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pswisshw14code.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pswisshw14code.x.tar
